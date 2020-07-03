2bbdb4bc1a3dc43dcdd6ffc08ee2ee5939a0308b
Victor Aguiar <vhaguiar@gmail.com>
2020-07-03 17:25:54 -0400

    corrections of Det Rational
    for it to work on stable version

--- Deterministic_test/RAT_deterministic_test.jl
+++ Deterministic_test/RAT_deterministic_test.jl
@@ -45,41 +45,48 @@ p=rho[ind,:,:]
 q=cve[ind,:,:]
 
 function rdmatrix(;p=p,q=q)
-rdmatrixfill=zeros(T,T)
-for t=1:T, s=1:T
-  if (p[t,:]'*q[t,:])[1]>=(p[t,:]'*q[s,:])[1]
-    rdmatrixfill[t,s]=1
-  end
-end
-rdmatrixfill
+    rdmatrixfill=zeros(T,T)
+    for t=1:T, s=1:T
+      if (p[t,:]'*q[t,:])[1]>=(p[t,:]'*q[s,:])[1]
+        rdmatrixfill[t,s]=1
+      end
+    end
+    rdmatrixfill
 end
 
 function tclosure(;m=m)
-n0=size(m)[2]
-for k=1:n0, i=1:n0, j=1:n0
-  if  m[i,k]==1 && m[k,j]==1
-    m[i,j]=1
-  end
-end
+    n0=size(m)[2]
+    for k=1:n0, i=1:n0, j=1:n0
+      if  m[i,k]==1 && m[k,j]==1
+        m[i,j]=1
+      end
+    end
+    m
 end
 
 function garpok(;p=p,q=q)
-garpmatrixfill=zeros(T,T)
-m=tclosure(m=rdmatrix(p=p,q=q))
-for t=1:T, s=1:T
-  if m[t,s]==1 && ((p[s,:]'*q[s,:])[1]>(p[s,:]'*q[t,:])[1])
-    garpmatrixfill[t,s]=1
-  end
-end
-maximum(garpmatrixfill)
-## output of zero is a pass, no violation of GARP
+    garpmatrixfill=zeros(T,T)
+    m=tclosure(m=rdmatrix(p=p,q=q))
+    for t=1:T
+        for s=1:T
+            if m[t,s]==1 && ((p[s,:]'*q[s,:])[1]>(p[s,:]'*q[t,:])[1])
+                garpmatrixfill[t,s]=1
+            end
+        end
+    end
+
+    maximum(garpmatrixfill)
+    ## output of zero is a pass, no violation of GARP
 end
 
-garpresults=zeros(n)
-for ind=1:n
-  garpresults[ind]=garpok(p=rho[ind,:,:],q=cve[ind,:,:])
+function testgarp(;rho=rho,cve=cve)
+    garpresults=zeros(n)
+    for ind=1:n
+      garpresults[ind]=garpok(p=rho[ind,:,:],q=cve[ind,:,:])
+    end
+    garpresults
 end
-
+garpresults=testgarp(rho=rho,cve=cve)
 rate=1-sum(garpresults)/n
 
 ## Pass Rate
