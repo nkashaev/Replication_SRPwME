846f2890e5481da38b891b9c52ff3cda2ddc8eeb
vhaguiar <vhaguiar@gmail.com>
2020-07-03 16:52:18 -0400

    replication files for julia packages
    These are replications files
    Manifest and Project should be frozen

---
+++ AKpackages2.jl
@@ -0,0 +1,22 @@
+using Pkg
+Pkg.add("NLopt")
+Pkg.add("LinearAlgebra")
+Pkg.add("Random")
+Pkg.add("MathProgBase")
+Pkg.add("Clp")
+Pkg.add("DataFrames")
+Pkg.add("JuMP")
+Pkg.add("Convex")
+Pkg.add("ECOS")
+Pkg.add("CSV")
+Pkg.add("Distributions")
+Pkg.add("BlackBoxOptim")
+Pkg.add("SCS")
+Pkg.add("Ipopt")
+Pkg.add("SoftGlobalScope")
+
+
+### CUDA
+Pkg.add("CuArrays")
+Pkg.add("CUDAnative")
+Pkg.add("CUDAdrv")
