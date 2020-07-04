27f21ce7c0fc1b48972f7101c04c8bf93dc0a553
nkashaev <36710350+nkashaev@users.noreply.github.com>
2020-07-04 18:07:35 -0400

    Change in cuda_nail

--- Test_bbqa/test_bbqa.jl
+++ Test_bbqa/test_bbqa.jl
@@ -1,38 +1,71 @@
-using LinearAlgebra
-using Random
-using MathProgBase
-using Clp
-using DataFrames
-using CSV
+# using LinearAlgebra
+# using Random
+# using MathProgBase
+# using Clp
+# using DataFrames
+# using CSV
 using NLopt
-using BlackBoxOptim
-
- global count1=0
-
-guessgamma=[-1.0,1.0, 1.0]
-dg=length(guessgamma)
-opt=NLopt.Opt(:LN_BOBYQA,dg)
-toluser=1e-4
-NLopt.lower_bounds!(opt,ones(dg).*-Inf)
-NLopt.upper_bounds!(opt,ones(dg).*Inf)
-NLopt.xtol_rel!(opt,toluser)
-NLopt.min_objective!(opt,objMCcu)
-(minf,minx,ret) = NLopt.optimize!(opt, guessgamma)
-guessgamma=minx
-(minf,minx,ret) = NLopt.optimize!(opt, guessgamma)
-println(minf)
-guessgamma=minx
-(minf,minx,ret) = NLopt.optimize!(opt, guessgamma)
-println(minf)
-println(ret)
-println(count)
-
+#using BlackBoxOptim
 function objMCcu(gamma0::Vector, grad::Vector)
   if length(grad) > 0
   end
   global count1
     count1::Int += 1
-    println("f_$count1($gamma0)")
 
   return 100.0 * (gamma0[2] - gamma0[1]^2)^2 + (1.0 - gamma0[1])^2+(gamma0[3] - gamma0[1]^2)^2
 end
+
+global count1=0
+toluser=1e-10
+guessgamma=[-1.0,1.0, 1.0]
+dg=length(guessgamma)
+opt=NLopt.Opt(:LN_BOBYQA,dg)
+NLopt.lower_bounds!(opt,ones(dg).*-Inf)
+NLopt.upper_bounds!(opt,ones(dg).*Inf)
+NLopt.xtol_rel!(opt,toluser)
+NLopt.min_objective!(opt,objMCcu)
+(minf,minx,ret) = NLopt.optimize!(opt, guessgamma)
+println(minf)
+
+global count1=0
+
+guessgamma=[-1.0,1.0, 1.0]
+dg=length(guessgamma)
+opt=NLopt.Opt(:LN_BOBYQA,dg)
+NLopt.lower_bounds!(opt,ones(dg).*-Inf)
+NLopt.upper_bounds!(opt,ones(dg).*Inf)
+NLopt.xtol_rel!(opt,toluser)
+NLopt.min_objective!(opt,objMCcu)
+(minf,minx,ret) = NLopt.optimize!(opt, guessgamma)
+println(minf)
+
+global count1=0
+
+guessgamma=[-1.0,1.0, 1.0]
+dg=length(guessgamma)
+opt=NLopt.Opt(:LN_BOBYQA,dg)
+NLopt.lower_bounds!(opt,ones(dg).*-Inf)
+NLopt.upper_bounds!(opt,ones(dg).*Inf)
+NLopt.xtol_rel!(opt,toluser)
+NLopt.min_objective!(opt,objMCcu)
+(minf,minx,ret) = NLopt.optimize!(opt, guessgamma)
+println(minf)
+guessgamma=minx
+(minf,minx,ret) = NLopt.optimize!(opt, guessgamma)
+println(minf)
+guessgamma=minx
+(minf,minx,ret) = NLopt.optimize!(opt, guessgamma)
+println(minf)
+guessgamma=minx
+(minf,minx,ret) = NLopt.optimize!(opt, guessgamma)
+println(minf)
+println(NLOPT_VERSION)
+
+# guessgamma=[-1.0,1.0, 1.0]
+# (minf,minx,ret) = NLopt.optimize!(opt, guessgamma)
+# println(minf)
+# guessgamma=minx
+# (minf,minx,ret) = NLopt.optimize!(opt, guessgamma)
+# println(minf)
+#
+# println(count1)
