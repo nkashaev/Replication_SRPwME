d1649e6bce29ca6bd5461526937afa221b2f59c5
vhaguiar <vhaguiar@gmail.com>
2020-07-04 09:22:45 -0400

    add Income Uncertainty
    income uncertainty files

---
+++ Appendix_E/E1_incomeuncertainty/cpufunctions/myfun_IU_mean.jl
@@ -0,0 +1,16 @@
+function myfun(;d=d,gamma=gamma,Lambda=Lambda,W=W,cve=cve,rho=rho)
+
+    gvec=ones(n,dg)
+
+
+    @simd for j=1:(T)
+      @inbounds gvec[:,j]=(sum((W[:,j,:]).*(rho[:,j,:]),dims=2))
+    end
+
+    gvec[:,T+1]=Lambda[:,2] .-1
+    gvec[:,T+2]=Lambda[:,3] .-1
+    gvec[:,T+3]=Lambda[:,4] .-1
+    #gvec[:,T+4]=(Lambda[:,4]-Lambda[:,3]).*Lambda[:,3]
+    #gvec[:,T+5]=(Lambda[:,3]-Lambda[:,2]).*Lambda[:,2]
+    gvec
+end
