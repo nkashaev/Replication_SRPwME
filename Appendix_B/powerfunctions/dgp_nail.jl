51e259e0ec332c9d8903766bce6eccb104924253
nkashaev <36710350+nkashaev@users.noreply.github.com>
2020-07-08 17:19:50 -0400

    Update dgp_nail.jl

--- Appendix_B/powerfunctions/dgp_nail.jl
+++ Appendix_B/powerfunctions/dgp_nail.jl
@@ -1,13 +1,17 @@
 function dgp12(ri,dlow,n,DP)
 (n0,T,K)=size(DP)
 Random.seed!(123*ri)
+#Prices
 rho=DP[rand(1:n0,n),:,:]
+#Discount factors
 deltasim=rand(n).*(1-dlow).+dlow; lambda=randexp(n)/1
+#Elasticities
 sl=1/15; su=100;
 sigma=rand(n,K)*(su-sl) .+ sl
 ##Multiplicative Error
 adum=0.97; bdum=1.03;
 epsilon=rand(n,T,K)*(bdum-adum) .+ adum
+#Consumption
 cve=zeros(n,T,K)
 for i=1:n, t=1:T, k=1:K
       cve[i,t,k]= ((lambda[i]/deltasim[i]^(t-1))*rho[i,t,k]).^(-1/sigma[i,k])*epsilon[i,t,k]
@@ -20,20 +24,23 @@ function dgp34(ri,mu_par,n,DP)
 #mu_par=[0.5, 0.5] or mu=[1/3, 2/3]
 (n0,T,K)=size(DP)
 Random.seed!(123*ri)
+#Prices
 rho=DP[rand(1:n0,n),:,:]
+#Discount factors
 dlowa=.1; dlowb=.99;
 deltasima=rand(n).*(1-dlowa) .+dlowa
 deltasimb=rand(n).*(1-dlowb) .+dlowb
 lambda=randexp(n); lambdab=randexp(n)
 #### Pareto Weights
 mu=rand(n,T,K).*(mu_par[2]-mu_par[1]) .+ mu_par[1]
+#Elasticities
 sl=1/15; su=100;
 sigma=rand(n,K)*(su-sl) .+ sl
 sigmab=rand(n,K)*(su-sl) .+ sl
-
-##
+#Multiplicative Error
 adum=0.97; bdum=1.03;
 epsilon=rand(n,T,K)*(bdum-adum) .+ adum
+#Consumption
 cve=zeros(n,T,K)
 @simd for i=1:n, t=1:T, k=1:K
        rhoadum=mu[i,t,k]*rho[i,t,k]
