eed74e94a3a2f463200e8a322aa51e32c936efed
Victor Aguiar <vhaguiar@gmail.com>
2020-07-22 12:39:51 -0400

    Update Powershell_autom.ps1
    powe shell

--- Powershell_autom.ps1
+++ Powershell_autom.ps1
@@ -1,3 +1,13 @@
-﻿
+﻿$WarningPreference = "SilentlyContinue"
+$VerbosePreference="Continue"
+Start-Transcript
+##Couples 
 
-D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\couples\AK_FirstApp_couples_theta0_01.jl"
+#D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\couples\AK_FirstApp_couples_theta0_01.jl"
+
+Measure-Command { D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\couples\AK_FirstApp_couples_theta0_05.jl"}
+
+D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\couples\AK_FirstApp_couples_theta0_09.jl"
+
+D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\couples\AK_FirstApp_couples_theta0_1.jl"
+
