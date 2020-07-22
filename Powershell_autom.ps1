485bd73f9e03d49d9cf48ca45d450b35f8ed61c0
vhaguiar <vhaguiar@gmail.com>
2020-07-22 14:25:54 -0400

    Update Powershell_autom.ps1
    update of powershell

--- Powershell_autom.ps1
+++ Powershell_autom.ps1
@@ -1,7 +1,22 @@
 ﻿$WarningPreference = "SilentlyContinue"
 $VerbosePreference="Continue"
 Start-Transcript
-##Couples 
+##Singles
+
+
+Measure-Command { D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\singles\AK_FirstApp_singles_theta0_0975.jl"}
+D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\singles\AK_FirstApp_singles_theta0_1.jl"
+D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\singles\AK_FirstApp_singles_theta0_01.jl"
+D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\singles\AK_FirstApp_singles_theta0_05.jl"
+D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\singles\AK_FirstApp_singles_theta0_09.jl"
+
+##Singles Recover Delta
+Measure-Command { D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\singles\AK_recover_ADF_997.jl"}
+D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\singles\AK_recover_ADF_996.jl"
+D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\singles\AK_recover_ADF_995.jl"
+D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\singles\AK_recover_ADF_994.jl"
+
+##Couples
 
 D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\couples\AK_FirstApp_couples_theta0_01.jl"
 
@@ -10,4 +25,3 @@ Measure-Command { D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHu
 D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\couples\AK_FirstApp_couples_theta0_09.jl"
 
 D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\couples\AK_FirstApp_couples_theta0_1.jl"
-
