daeae50dfc2fd9caa266820cc5573c507e67ffa5
Victor Aguiar <vhaguiar@gmail.com>
2020-07-22 14:02:28 -0400

    couples theta0=0.5
    first value

--- Powershell_autom.ps1
+++ Powershell_autom.ps1
@@ -3,7 +3,7 @@ $VerbosePreference="Continue"
 Start-Transcript
 ##Couples 
 
-#D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\couples\AK_FirstApp_couples_theta0_01.jl"
+D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\couples\AK_FirstApp_couples_theta0_01.jl"
 
 Measure-Command { D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\couples\AK_FirstApp_couples_theta0_05.jl"}
 
