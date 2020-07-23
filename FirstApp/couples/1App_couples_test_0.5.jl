3f4a68222aee58f53cd64c34c4a8ad577a81d3a1
nkashaev <36710350+nkashaev@users.noreply.github.com>
2020-07-22 22:19:19 -0400

    uniue test file

--- FirstApp/couples/1App_couples_test_0.5.jl
+++ FirstApp/couples/1App_couples_test_0.5.jl
@@ -1,6 +1,9 @@
 ## Lower bound for the support of the discount factor
 theta0=0.5
-
+## Sample size
+const n=2004
+#Type of households
+household="couples"
 ## Setting-up directory
 tempdir1=@__DIR__
 repdir=tempdir1[1:findfirst("ReplicationAK",tempdir1)[end]]
@@ -9,5 +12,10 @@ rootdir=repdir*"/"*appname
 diroutput=repdir*"/Output_all"
 dirdata=repdir*"/Data_all"
 ################################################################################
-include(rootdir*"/couples/1App_couples_test.jl")
+include(rootdir*"/cpufunctions/1App_test.jl")
+
+##Saving the output
+Results1=DataFrame([theta0 TSMC])
+names!(Results1,Symbol.(["theta0","TS"]))
+CSV.write(diroutput*"/AK_FirstApp_couples_TS_theta0_$theta0.csv",Results1)
 println("Hi Victor, I'm done")
--- FirstApp/singles/1App_singles_test_0.5.jl
+++ FirstApp/singles/1App_singles_test_0.5.jl
@@ -1,8 +1,9 @@
-###Author: Victor H. Aguiar and Nail Kashaev
-###email: vhaguiar@gmail.com
 ## Lower bound for the support of the discount factor
 theta0=0.5
-
+## Sample size
+const n=185
+#Type of households
+household="singles"
 ## Setting-up directory
 tempdir1=@__DIR__
 repdir=tempdir1[1:findfirst("ReplicationAK",tempdir1)[end]]
@@ -11,5 +12,10 @@ rootdir=repdir*"/"*appname
 diroutput=repdir*"/Output_all"
 dirdata=repdir*"/Data_all"
 ################################################################################
-include(rootdir*"/singles/1App_singles_test.jl")
+include(rootdir*"/cpufunctions/1App_test.jl")
+
+##Saving the output
+Results1=DataFrame([theta0 TSMC])
+names!(Results1,Symbol.(["theta0","TS"]))
+CSV.write(diroutput*"/AK_FirstApp_singles_TS_theta0_$theta0.csv",Results1)
 println("Hi Victor, I'm done")
