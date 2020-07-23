ef2745b663cd745a24ef0e9b30ff452f07362de9
Victor Aguiar <vhaguiar@gmail.com>
2020-07-23 07:57:50 -0400

    first run of v1.0
    commit to new values

--- Powershell_AK.ps1
+++ Powershell_AK.ps1
@@ -5,7 +5,7 @@ Start-Transcript
 Write-Host "SINGLES"
 
 Measure-Command { D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\singles\1App_singles_main_0.975.jl"}
-D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\singles\1App_singles_main_0.1.jl.jl"
+D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\singles\1App_singles_main_0.1.jl"
 D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\singles\1App_singles_main_0.5.jl"
 D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\singles\1App_singles_main_0.9.jl"
 D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\FirstApp\singles\1App_singles_main_1.0.jl"
