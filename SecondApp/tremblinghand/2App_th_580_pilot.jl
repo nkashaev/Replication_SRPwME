f03f8dd0a2c26b0854b49da23d44e2a32c164618
vhaguiar <vhaguiar@gmail.com>
2020-07-27 16:31:57 -0400

    Update 2App_th_580_pilot.jl
    commit small differences due to bobyqa

--- SecondApp/tremblinghand/2App_th_580_pilot.jl
+++ SecondApp/tremblinghand/2App_th_580_pilot.jl
@@ -1,6 +1,7 @@
 count = 0
 #Set the number of processors: Change it to the max the computer allows
-nprocs=20
+#nprocs=20
+nprocs=580
 using Distributed
 addprocs(nprocs)
 @everywhere Distributed
@@ -30,7 +31,8 @@ end
 ## Because the simulations are done using parallel Montecarlo we have nsimps*nprocs draws.
 # If needed, one can set burnrate.
 burnrate=0
-nsimsp=29
+#nsimsp=29
+nsimsp=1
 ## Running the code.
 include(rootdir*"/tremblinghand/2App_th_main.jl")
 
