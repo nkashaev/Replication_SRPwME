806018f48bc9cc2b1f121486220350bc04520206
Victor Aguiar <vhaguiar@gmail.com>
2020-07-27 18:51:03 -0400

    collective replicates
    check

--- Powershell_AK_appendix.ps1
+++ Powershell_AK_appendix.ps1
@@ -3,8 +3,7 @@ $VerbosePreference="Continue"
 Start-Transcript
 ##Singles
 Write-Host "COLLECTIVE"
-
-D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\Appendix_E\E2_collective\AK_collective.jl"
+D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\Appendix_E\Appendix_E2\AK_collective.jl"
 #Measure-Command { D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\Appendix_E\E2_collective\AK_collective.jl"}
 
 Write-Host "INCOME UNCERTAINTY"
