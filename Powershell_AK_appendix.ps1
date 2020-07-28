a0ce7920c60c6d021f54b677467ee13cf10f675c
Victor Aguiar <vhaguiar@gmail.com>
2020-07-27 20:10:33 -0400

    Update Powershell_AK_appendix.ps1
    income uncertainty

--- Powershell_AK_appendix.ps1
+++ Powershell_AK_appendix.ps1
@@ -3,10 +3,11 @@ $VerbosePreference="Continue"
 Start-Transcript
 ##Singles
 Write-Host "COLLECTIVE"
-D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\Appendix_E\Appendix_E2\AK_collective.jl"
-#Measure-Command { D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\Appendix_E\E2_collective\AK_collective.jl"}
+#D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\Appendix_E\Appendix_E2\AK_collective.jl"
+Measure-Command { D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\Appendix_E\Appendix_E2\AK_collective.jl"}
 
 Write-Host "INCOME UNCERTAINTY"
+Measure-Command { D:\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\Appendix_E\Appendix_E1\AK_IncomeUncertainty.jl"}
 
 ##Singles Recover Delta
 
