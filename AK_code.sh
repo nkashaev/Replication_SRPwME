6e3f9955e46a676fe24c4fcb99fcfd656540605e
nkashaev <36710350+nkashaev@users.noreply.github.com>
2020-07-31 14:35:18 -0400

    update the cycles

--- AK_code.sh
+++ AK_code.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-cd `dirname $0`
 
 K='1 2 3'
+B='4 5 6'
 for k in $K
 do
-julia nail_test.jl $k
+for b in $B
+do
+julia nail_test.jl $k $b
+done
 done
--- nail_test.jl
+++ nail_test.jl
@@ -1,1 +1,1 @@
-println(parse(Float64,ARGS[1])+1)
+println(parse(Float64,ARGS[2])*parse(Float64,ARGS[1]))
