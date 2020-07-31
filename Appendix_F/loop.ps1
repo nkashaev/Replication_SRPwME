92b04fbaf7f797d186e60117175f6b520541a127
vhaguiar <vhaguiar@gmail.com>
2020-07-31 15:46:22 -0400

    Update loop.ps1
    fixed loop

--- Appendix_F/loop.ps1
+++ Appendix_F/loop.ps1
@@ -8,5 +8,6 @@ for (($ki = 1); $ki -lt 11+1; $ki++)
     {
         "`$ki:$ki"
         "`$ri:$ri"
+        C:\Users\vaguiar\AppData\Local\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\Appendix_F\F_main_shell.jl" $ki $ri
     }
 }
