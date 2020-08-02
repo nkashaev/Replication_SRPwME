4f53e8feb2a874143a9939000dae8eefd604d89e
vhaguiar <vhaguiar@gmail.com>
2020-08-02 12:59:54 -0400

    Update loop.ps1
    delete line

--- Appendix_F/loop.ps1
+++ Appendix_F/loop.ps1
@@ -1,6 +1,5 @@
 ﻿
 
-C:\Users\vaguiar\AppData\Local\Julia-1.1.1\bin\julia.exe "C:\Users\vaguiar\Documents\GitHub\ReplicationAK\Appendix_F\F_main_shell.jl" 1 1
 
 for (($ki = 1); $ki -lt 11+1; $ki++)
 {
