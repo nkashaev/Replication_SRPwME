daabd0b64abb25d6e55cb89afa0a5131be6f9c4a
vhaguiar <vhaguiar@gmail.com>
2020-08-02 16:24:41 -0400

    -eps
    new

--- Appendix_F/F_figure1.R
+++ Appendix_F/F_figure1.R
@@ -50,7 +50,7 @@ fig1=ggplot(figdf, aes(x=price-1, y=shares, group=type)) +
 fig1
 fig1 +labs(x=TeX("$\\kappa$"),y=TeX("$\\theta_{pet}$"))+ theme_minimal()
 
-pdf(paste(filefigure,"\\fig1_",theta0str,"_minimal.EPS",sep=""),width =10,height =2)
+pdf(paste(filefigure,"\\fig1_",theta0str,"_minimal.eps",sep=""),width =10,height =2)
 myplot <- fig1 +labs(x=expression(kappa),y=expression(theta[pet])) + theme_minimal()
 print(myplot)
 dev.off()
