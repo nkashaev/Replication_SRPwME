628bc460b80b882e95443bd4eca524c112b19c73
nkashaev <36710350+nkashaev@users.noreply.github.com>
2020-08-03 16:12:07 -0400

    1

--- OldFiles/pvalues.jl
+++ OldFiles/pvalues.jl
@@ -23,3 +23,15 @@ using Distributions
 #Collective
 1-cdf(Chisq(4),0.017779151532162054)
 quantile(Chisq(4),0.95)
+
+using CSV
+P1=Array(CSV.read("/Users/nailkashaev/Dropbox/GitHub/ReplicationAK/Output_all/Appendix/F_1.0._0.975.csv"))
+P2=Array(CSV.read("/Users/nailkashaev/Dropbox/GitHub/ReplicationAK/Output_all/Appendix/F_1.1._0.975.csv"))
+
+crval=quantile(Chisq(5),.95) #critical values based on chi2(4)
+
+I1=(P1[:,2].<crval)
+I2=(P2[:,2].<crval)
+
+println(P1[I1,:])
+println(P2[I2,:])
