d8e7983ae60f51d16074b2bf213a8e6c69b44d5d
nkashaev <36710350+nkashaev@users.noreply.github.com>
2020-08-28 10:23:46 -0400

    Moved rawdata manipulation to Data_all

---
+++ Data_all/rawdata/2app_prepare_data.R
@@ -0,0 +1,17 @@
+### R 3.2.2
+require(R.matlab)
+
+##input files
+dir="C:\\Users\\pegas\\Documents\\GitHub\\ReplicationAK\\Data_all\\rawdata"
+Data.file=paste(dir,"\\","AllData.mat",sep="")
+
+## output files
+dirout="C:\\Users\\pegas\\Documents\\GitHub\\ReplicationAK\\Data_all"
+OutData.file=paste(dirout,"\\","rationalitydata3goods.csv",sep="")
+
+Data=readMat(Data.file)
+
+OutData=cbind(1:dim(Data$AllData)[1],Data$AllData,round(100/Data$AllData[,6],digits=9),round(100/Data$AllData[,7],digits=9),round(100/Data$AllData[,8],digits=9))
+colnames(OutData)=c("","id","obs","x","y","z","xa","ya","za","px","py","pz")
+
+write.csv(OutData,file=OutData.file,row.names=FALSE)
