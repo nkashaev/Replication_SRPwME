5dea282ced04dd75bb6b3854250c93ec437ad83d
vhaguiar <vhaguiar@gmail.com>
2020-08-27 21:21:28 -0400

    2app digits round to 8 digits

--- datamanipulation/2app_prepare_data.R
+++ datamanipulation/2app_prepare_data.R
@@ -9,6 +9,6 @@ OutData.file=paste(dir,"\\","rationalitydata3goods.csv",sep="")
 
 Data=readMat(Data.file)
 
-OutData=cbind(1:dim(Data$AllData)[1],Data$AllData,100/Data$AllData[,6],100/Data$AllData[,7],100/Data$AllData[,8])
+OutData=cbind(1:dim(Data$AllData)[1],Data$AllData,round(100/Data$AllData[,6],digits=8),round(100/Data$AllData[,7],digits=8),round(100/Data$AllData[,8],digits=8))
 
 write.csv(OutData,file=OutData.file,row.names=FALSE,col.names=NA)
