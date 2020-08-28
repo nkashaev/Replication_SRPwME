41746289f054e48209b7930284be788e79da583f
vhaguiar <vhaguiar@gmail.com>
2020-08-27 21:14:04 -0400

    2 App

--- datamanipulation/2app_prepare_data.R
+++ datamanipulation/2app_prepare_data.R
@@ -9,4 +9,6 @@ OutData.file=paste(dir,"\\","rationalitydata3goods.csv",sep="")
 
 Data=readMat(Data.file)
 
-OutData=cbind(Data$AllData,100/Data$AllData[,6],100/Data$AllData[,7],100/Data$AllData[,8])
+OutData=cbind(1:dim(Data$AllData)[1],Data$AllData,100/Data$AllData[,6],100/Data$AllData[,7],100/Data$AllData[,8])
+
+write.csv(OutData,file=OutData.file,row.names=FALSE,col.names=NA)
